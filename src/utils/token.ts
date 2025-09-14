b33e706419c3f4100417506666ef841aa96813d6
Danil <posikunets@yandex.ru>
2025-09-14 15:17:51 +0300

    fix token utils

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,15 +1,14 @@
 import { TAccessTokenBody } from '@/db/sessions/types';
 import { UUID } from 'crypto';
-import { NextFunction, RequestHandler, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import jwt, { SignOptions } from 'jsonwebtoken';
 import { StringValue } from 'ms';
-import { CustomError, ERROR_MESSAGE } from './service';
-import { getSessionByID } from '@/db/sessions/sessions';
-import { getEmailByID } from '@/db/emails/emails';
-import { sendAlertMail } from '@/transporter';
+import { CustomError } from './service';
+import { getSessionByID } from '@/db/sessions';
+import { ERROR_MESSAGE } from '@/data/constans';
 
-export const createToken = <T extends object>(
-  payload: T,
+export const createToken = (
+  payload: object,
   key: UUID,
   expriresIn: number | StringValue | undefined,
 ): string => {
@@ -24,22 +23,31 @@ export const getTokenPayload = <T>(token: string): T => {
   return jwt.decode(token) as T;
 };
 
-export const verifyTokenWithResponse = async (
+export const verifyTokenHandler: RequestHandler = async (
+  req: Request,
   res: Response,
-  token: string,
   next?: NextFunction,
 ): Promise<void> => {
   try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+      CustomError(res, 401);
+      return;
+    }
+
     const { id, sessionID } = getTokenPayload<TAccessTokenBody>(token);
 
     if (!id || !sessionID) {
-      return CustomError(res, 400, 'Токен не содержит необходимой информации.');
+      CustomError(res, 401);
+      return;
     }
 
     const session = await getSessionByID(sessionID);
 
     if (!session) {
-      return CustomError(res, 401, 'Токен не дейстивтелен.');
+      CustomError(res, 401);
+      return;
     }
 
     jwt.verify(token, session.key);
@@ -50,28 +58,10 @@ export const verifyTokenWithResponse = async (
   } catch (error) {
     const err = error as Error;
 
-    if (err.message === 'jwt expired') {
-      return CustomError(res, 400, 'Токен не дейстивтелен.');
-    }
-
     if (err.message === 'invalid signature') {
-      return CustomError(res, 400, 'Ошибка авторизации.', err);
+      console.log('INVALID_SIGNATURE', req);
     }
 
-    return CustomError(res, 500, ERROR_MESSAGE, err);
+    CustomError(res, 500, ERROR_MESSAGE, err);
   }
-};
-
-export const checkAccessTokenHandler: RequestHandler = async (
-  req,
-  res,
-  next,
-) => {
-  const auth = req.headers.authorization;
-
-  if (!auth) {
-    return CustomError(res, 400, 'Вы не авторизованы.');
-  }
-
-  await verifyTokenWithResponse(res, auth, next);
 };
